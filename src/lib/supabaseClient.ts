3e75c9ca92515109db215aeaf3034a31d5f79d08
0SalvadOr0 <grizzzaffigiovy219@gmail.com>
2025-08-04 12:26:34 +0200

    aggiustamenti

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-import env from "react-dotenv";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASEURL; // Sostituisci con il tuo URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASEANONKEY; // Sostituisci con la tua anon key
